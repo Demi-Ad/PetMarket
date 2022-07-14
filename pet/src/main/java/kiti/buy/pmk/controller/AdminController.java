bbd2bdcb5db4f7d6cac010161c3833f707f35c7e
kimminhyuck <kiti98261@naver.com>
2022-07-14 11:23:01 +0900

    merge

--- pet/src/main/java/kiti/buy/pmk/controller/AdminController.java
+++ pet/src/main/java/kiti/buy/pmk/controller/AdminController.java
@@ -1,12 +1,18 @@
 package kiti.buy.pmk.controller;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import kiti.buy.pmk.dto.admin.AdminLoginDTO;
+import kiti.buy.pmk.dto.admin.AdminMemberDTO;
 
 import kiti.buy.pmk.service.AdminService;
 import kiti.buy.pmk.vo.AdminVO;
@@ -20,14 +26,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminController {
 
-	
-	private final AdminService adminservice;
+	private final AdminService adminService;
 
 	@GetMapping("/admin/login")
 	public String adminLoginView() {
 		return "admin/admin_login";
 	}
+
+	@PostMapping("/admin/login")
+	public String adminLogin(@ModelAttribute AdminLoginDTO dto, HttpSession session) {
+		 AdminVO admin = adminService.login(dto);
+		 if (admin == null) {
+			 return "admin/admin_login";
+		 }
+		 
+		 session.setAttribute("session_detail", admin);
+		 
+		 return "redirect:/admin/dash";
+	}
+	
+	@GetMapping("/admin/dash")
+	public String dashForm() {
+		return "admin/admindash";
+	}
+	
+	
 	
 	
 
 }
+
--- pet/src/main/java/kiti/buy/pmk/service/AdminService.java
+++ pet/src/main/java/kiti/buy/pmk/service/AdminService.java
@@ -1,8 +1,47 @@
 package kiti.buy.pmk.service;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kiti.buy.pmk.dto.admin.AdminLoginDTO;
+import kiti.buy.pmk.dto.admin.AdminMemberDTO;
+import kiti.buy.pmk.mapper.AdminMapper;
+import kiti.buy.pmk.vo.AccountVO;
+import kiti.buy.pmk.vo.AdminVO;
+import lombok.RequiredArgsConstructor;
 
 @Service
 public class AdminService {
+	
+	private AdminMapper adminMapper;
+	
+	
+	@Autowired
+	public AdminService(AdminMapper adminMapper) {
+		this.adminMapper = adminMapper;
+	}
+
+
+
+	public AdminVO login(AdminLoginDTO dto) {
+		
+		return adminMapper.adminLogin(dto);
+		
+	}
+	
+	
+	
+	
+	
+	public void adminMemberOut(AdminMemberDTO dto) throws Exception{
+		
+	}
+
 
 }
+
+
